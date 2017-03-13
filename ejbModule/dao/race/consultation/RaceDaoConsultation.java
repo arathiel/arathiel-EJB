e5975d4f069166c7f22b3a61a54bbec547d600c1
Francois Georgel <fgeorgel@club-internet.fr>
2017-03-13 13:10:13 +0100

    mise en place des classes dao pour race

---
+++ ejbModule/dao/race/consultation/RaceDaoConsultation.java
@@ -0,0 +1,18 @@
+package dao.race.consultation;
+
+/**
+ * Cette classe gère les requêtes à la base de donnée pour:
+ * 
+ * 	- 	La création
+ * 	-	La suppression
+ *  -	La modification 
+ *  
+ *  des races du jeu
+ * 
+ * @author Francois Georgel
+ *
+ */
+
+public class RaceDaoConsultation {
+
+}

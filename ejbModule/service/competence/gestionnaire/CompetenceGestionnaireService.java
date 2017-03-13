e9c8301a6b2656072ad2130ab0e12a4399477be9
Claire <de.filippis.claire@openmailbox.org>
2017-03-13 11:40:08 +0100

    Add JavaDoc & EJB DAO

---
+++ ejbModule/service/competence/gestionnaire/CompetenceGestionnaireService.java
@@ -0,0 +1,27 @@
+package service.competence.gestionnaire;
+
+import javax.ejb.EJB;
+import javax.ejb.LocalBean;
+import javax.ejb.Singleton;
+
+import dao.FacadeDAO;
+
+/**
+ * Cette classe gère toute les demandes de gestion du module Compétence.
+ * 
+ * Elle interroge dao.FacadeDAO.class pour toutes requêtes à la base de données.
+ * Elle est instanciée par le serveur d'application à l'appel de service.FacadeServiceCompetence.class
+ * Une fois créé, le même objet est utilisé.
+ * 
+ * @author Claire
+ * @version 20170313
+ *
+ */
+@LocalBean
+@Singleton
+public class CompetenceGestionnaireService {
+
+	@EJB
+	private FacadeDAO dao;
+	
+}

d0157e5adb63afbcf732a1a2951d0a015781df8c
Claire <de.filippis.claire@openmailbox.org>
2017-03-13 11:08:44 +0100

    Create FacadeService base.

--- ejbModule/clientServeur/IFacadeService.java
+++ ejbModule/clientServeur/IFacadeService.java
@@ -1,5 +1,14 @@
 package clientServeur;
 
+/**
+ * Interface pour service.FacadeService.class
+ * 
+ * Contient toutes les méthodes disponibles pour le serveur web.
+ * 
+ * @author Groupe
+ * @version 20170313
+ *
+ */
 public interface IFacadeService {
 
 }
---
+++ ejbModule/service/FacadeService.java
@@ -0,0 +1,14 @@
+package service;
+
+/**
+ * Implémentation de clientServeur.IFacadeService.class
+ * 
+ * Configuration de toutes les méthodes disponibles pour le serveur web.
+ * 
+ * @author Groupe
+ * @version 20170313
+ *
+ */
+public class FacadeService {
+
+}

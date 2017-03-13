f2ca1bad4f857d9ccbabb130d1fb454e9aaf6fd8
Claire <de.filippis.claire@openmailbox.org>
2017-03-13 11:40:20 +0100

    Create base

---
+++ ejbModule/dao/FacadeDAO.java
@@ -0,0 +1,17 @@
+package dao;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Singleton;
+
+/**
+ * Facade DAO.
+ * 
+ * @author Groupe
+ * @version 20170313
+ *
+ */
+@LocalBean
+@Singleton
+public class FacadeDAO {
+
+}

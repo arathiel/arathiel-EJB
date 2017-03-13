812986dcc7748583f44f37101ab1b16c0e2e49a4
jo <jo@mio>
2017-03-13 20:07:39 +0100

    Trait : création Description.java, modification Trait.java

--- ejbModule/entity/trait/Trait.java
+++ ejbModule/entity/trait/Trait.java
@@ -1,6 +1,11 @@
 package entity.trait;
 
+import javax.persistence.Column;
+import javax.persistence.Embedded;
 import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 
 import technic.trait.Comportements;
 
@@ -13,11 +18,28 @@ import technic.trait.Comportements;
 public class Trait {
 	
 	// Attributs de classe
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	@Column(name = "tr_id", length = 5)
 	private int				id;
+	
+	@Column(name = "tr_lib", length = 50, nullable = false)
 	private	String			libelle;
+	
+	@Column(name = "tr_pub", nullable = false)
 	private boolean 		visiPublic;
+	
+	@Column(name = "tr_crea", nullable = false)
 	private boolean			dispoCrea;
+	
+	@Column(name = "tr_malus", nullable = false)
 	private boolean			malus;
-	private Comportements	listComp; 
+	
+	@Column(name = "tr_comp")
+	private Comportements	listComp;
+	
+	@Embedded
+	@Column(name = "tr_desc")
+	private Description		description;
 
 }

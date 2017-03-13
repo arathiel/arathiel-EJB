582579da003fe42b2c9f4520fe08e30c1e29b981
Claire <de.filippis.claire@openmailbox.org>
2017-03-13 11:54:12 +0100

    Add JavaDoc & EJB Comp

--- ejbModule/dao/FacadeDAO.java
+++ ejbModule/dao/FacadeDAO.java
@@ -1,10 +1,17 @@
 package dao;
 
+import javax.ejb.EJB;
 import javax.ejb.LocalBean;
 import javax.ejb.Singleton;
 
+import dao.competence.FacadeDaoCompetence;
+
 /**
- * Facade DAO.
+ * Cette classe est la facade de la couche DAO.
+ * 
+ * Liste et aiguillage des méthodes interragissant avec l'unité de persistance.
+ * Elle est instanciée par le serveur d'application à l'appel des différentes classes du package service.
+ * Une fois créé, le même objet est utilisé.
  * 
  * @author Groupe
  * @version 20170313
@@ -14,4 +21,7 @@ import javax.ejb.Singleton;
 @Singleton
 public class FacadeDAO {
 
+	@EJB
+	private FacadeDaoCompetence daoComp;
+	
 }

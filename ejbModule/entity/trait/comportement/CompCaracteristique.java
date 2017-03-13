808470a6d855a70b898e9e6a79c0c2b7d45c8ff1
jo <jo@mio>
2017-03-13 20:25:01 +0100

    CompCaracteristique : Modification

--- ejbModule/entity/trait/comportement/CompCaracteristique.java
+++ ejbModule/entity/trait/comportement/CompCaracteristique.java
@@ -2,7 +2,10 @@ package entity.trait.comportement;
 
 import javax.persistence.Entity;
 
+import entity.caracteristique.Caracteristique;
+
 @Entity
 public class CompCaracteristique extends Technique {
 
+	private Caracteristique caracteristique;
 }

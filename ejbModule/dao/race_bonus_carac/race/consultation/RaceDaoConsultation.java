c8a14cd932745a0306a94842dbbb08e46d8a744f
Francois Georgel <fgeorgel@club-internet.fr>
2017-03-27 13:08:46 +0200

    ajout des classes de dao finales races

---
+++ ejbModule/dao/race_bonus_carac/race/consultation/RaceDaoConsultation.java
@@ -0,0 +1,110 @@
+package dao.race_bonus_carac.race.consultation;
+
+import java.util.ArrayList;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Singleton;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import dao.race_bonus_carac.exception.DaoExceptionRBCMsg;
+import dao.race_bonus_carac.param.Requetes;
+import dao.race_bonus_carac.exception.DaoExceptionRBC;
+import entity.race_bonus_carac.race.Race;
+
+/**
+ * Cette classe gère les requêtes à la base de donnée pour:
+ * 
+ * 	- 	Lister l'ensemble des races du jeu
+ *  -	Lister l'ensemble des races du jeu accessible par le joueur
+ * 	-	Rechercher une race par son id
+ *  -	Rechercher une race par son nom
+ *  
+ * 
+ * @author Francois Georgel
+ *
+ */
+
+
+@LocalBean
+@Singleton
+public class RaceDaoConsultation {
+	
+	@PersistenceContext(unitName="A1Persistence")
+	EntityManager em;
+	
+	/**
+	 * Liste de toutes les races insérées dans la base
+	 * 
+	 * 
+	 * @return ArrayList<Race>
+	 */
+	public ArrayList<Race> listeToutesRaces() {
+		ArrayList<Race> liste = new ArrayList<Race>();
+		
+		for (Object r : em.createQuery(Requetes.TOUTES_RACES.getMsg()).getResultList()) {
+			if (r instanceof Race){
+				liste.add((Race) r);
+			}
+		}
+		return liste;		
+	}
+	
+	
+	/**
+	 * Liste de toutes les races disponibles pour les joueurs
+	 * 
+	 * @return ArrayList<Race>
+	 */
+	public ArrayList<Race> listeRacesJouables() {
+		//TODO
+		return null;		
+	}
+
+	
+	/**
+	 * Recherche d'une Race par nom
+	 * 
+	 * @param nom
+	 * @return Race
+	 * @throws DaoExceptionRBC
+	 */
+	public Race RechRaceParNom(String nom) throws DaoExceptionRBC {
+		Race raceHib = null;
+		
+		if (nom!=null) {
+			nom = nom.trim().toLowerCase();	
+		
+			try {
+				raceHib = (Race) em.createQuery(Requetes.RACE_PAR_NOM.getMsg()).setParameter(1, nom).getSingleResult();
+				
+			} catch(Exception e) {
+				if (e instanceof javax.persistence.NoResultException) {
+					throw new DaoExceptionRBC(DaoExceptionRBCMsg.RACE_NO_EXIST);
+				}
+			}
+		}	
+		return raceHib;
+	}
+	
+	
+	/**
+	 * Recherche d'une race par son Id
+	 * 
+	 * @param id
+	 * @return Race
+	 * @throws DaoExceptionRBC
+	 */
+	public Race RechRaceParId(int id) throws DaoExceptionRBC {
+		Race raceHib = null;
+		
+		raceHib = (Race) em.find(Race.class, id);	
+
+		if (raceHib == null) {
+			throw new DaoExceptionRBC(DaoExceptionRBCMsg.RACE_NO_EXIST);
+		}
+		
+		return raceHib;
+	}
+
+}
---
+++ ejbModule/dao/race_bonus_carac/race/gestion/RaceDaoGestion.java
@@ -0,0 +1,152 @@
+package dao.race_bonus_carac.race.gestion;
+
+import java.util.regex.Pattern;
+
+import javax.ejb.EJB;
+import javax.ejb.LocalBean;
+import javax.ejb.Singleton;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import dao.race_bonus_carac.exception.DaoExceptionRBCMsg;
+import dao.race_bonus_carac.param.Requetes;
+import dao.race_bonus_carac.bonus.DaoBonus;
+import dao.race_bonus_carac.exception.DaoExceptionRBC;
+import dao.race_bonus_carac.race.consultation.RaceDaoConsultation;
+import entity.race_bonus_carac.bonus.Bonus;
+import entity.race_bonus_carac.race.Race;
+
+/**
+ * Cette classe gère les requêtes de persistence pour:
+ * 
+ * 	- 	La création
+ * 	-	La suppression
+ *  -	La modification 
+ *  
+ *  		des races du jeu
+ * 
+ * @author Francois Georgel
+ *
+ */
+
+@LocalBean
+@Singleton
+public class RaceDaoGestion {
+	
+	@EJB
+	RaceDaoConsultation rDaoConsult;
+	
+	@EJB
+	DaoBonus daoBonus;
+	
+	
+	@PersistenceContext(unitName="A1Persistence")
+	EntityManager em;
+	
+	/**
+	 * Insertion d'une race dans la base
+	 * 
+	 * @param race
+	 * @throws DaoExceptionRBC
+	 */
+	public void insertRace (Race race) throws DaoExceptionRBC {
+		//On teste d'abord si le nom communiqué est valide
+		raceNomValide(race);
+		
+		//On teste ensuite si parmis les bonus certains n'existent pas. On les insère d'abord.
+		for (Bonus b : race.getListeBonus()){
+			daoBonus.verifBonusPresent(b);
+		}	
+		
+		
+		try { 
+			em.persist(race);
+			em.flush();
+		} catch(Exception e) {
+			if (e.getCause() instanceof org.hibernate.exception.ConstraintViolationException) {
+				throw new DaoExceptionRBC(DaoExceptionRBCMsg.DOUBLON_NOM_RACE);
+			} else {
+				throw new DaoExceptionRBC(DaoExceptionRBCMsg.PB_INSERT_RACE);
+			}
+		}		
+	}
+	
+	
+	/**
+	 * Suppression d'une Race dans la base
+	 * 
+	 * @param race
+	 * @throws DaoExceptionRBC
+	 */
+	public void deleteRace(Race race) throws DaoExceptionRBC {	
+		Race raceHib = null;
+		
+		if (race != null) {
+			raceHib = rDaoConsult.RechRaceParId(race.getId());
+		}
+		
+		if (raceHib != null) {
+			//parcours de la table bonus_race pour y supprimer toutes les réferences à cette race
+			//le cascadeType.remove posée sur la liste de bonus de la race ne fonctionne pas car il cherche également à supprimer le bonus (qui peut être lié à d'autres races)
+			em.createNativeQuery(Requetes.DELETE_BONUS_RACE.getMsg()).setParameter(1, race.getId());
+			
+			//puis on supprime la race selectionnée		
+			em.remove(raceHib);
+			em.flush();
+			
+		} else {	
+			throw new DaoExceptionRBC(DaoExceptionRBCMsg.RACE_NO_EXIST);	
+		}	
+	
+	}
+	
+	/**
+	 * Mise à jour d'une race dans la base
+	 * 
+	 * 
+	 * @param race
+	 * @throws DaoExceptionRBC
+	 */
+	public void updateRace(Race race) throws DaoExceptionRBC{
+		//On cherche si le nom de la race à modifier est correct
+		raceNomValide(race);
+		
+		//On cherche si la race à modifier existe bien dans la base
+		@SuppressWarnings("unused")
+		Race raceHib;
+		try {
+			raceHib = rDaoConsult.RechRaceParId(race.getId());
+		} catch (DaoExceptionRBC e) {
+			if (e.getMessage().equals(DaoExceptionRBCMsg.RACE_NO_EXIST.getMsg())) { throw new DaoExceptionRBC(e.getMessage());}
+			else {throw new DaoExceptionRBC(DaoExceptionRBCMsg.PB_UPDATE_RACE);}
+		}
+			
+		//On retire auparavant les bonus correspondant à la race avant le merge
+		em.createNativeQuery(Requetes.DELETE_BONUS_RACE.getMsg()).setParameter(1, race.getId());
+		
+		//Puis on insère les nouveaux (en passant par une verification)
+		for (Bonus b : race.getListeBonus()){
+			try {
+				daoBonus.verifBonusPresent(b);
+			} catch (DaoExceptionRBC e) {
+				if (e.getMessage().equals(DaoExceptionRBCMsg.BONUS_NULL.getMsg())) 		{ throw new DaoExceptionRBC(DaoExceptionRBCMsg.BONUS_NULL);}
+				if (e.getMessage().equals(DaoExceptionRBCMsg.ERR_VAL_BONUS.getMsg())) 	{ throw new DaoExceptionRBC(DaoExceptionRBCMsg.ERR_VAL_BONUS);}
+				else { throw new DaoExceptionRBC(DaoExceptionRBCMsg.PB_INSERT_BONUS);}
+			}
+		}	
+		
+		em.merge(race);
+		em.flush();		
+	}
+		
+	//Methode qui va vérifier la validité du nom d'une race (ne contient pas de signes non autorisés et n'est pas null) 
+	public void raceNomValide(Race race) throws DaoExceptionRBC {
+	
+		boolean b = Pattern.matches("[[a-z] ' \\- () éèêëâàäïîôûüç]*", race.getNom().trim().toLowerCase());
+		
+		if (race.getNom().isEmpty() || race.getNom() == null || b==false || race.getNom().length() > 40){
+			throw new DaoExceptionRBC(DaoExceptionRBCMsg.RACE_NOM_INVALIDE);
+		}	
+	}
+	
+}

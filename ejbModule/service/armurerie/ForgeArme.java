ffdfb4d6a3ab3ff7a8a4d081edda5dc869ddba6e
OllyB <tyno0809@gmail.com>
2017-04-05 15:48:33 +0200

    creation Forge

---
+++ ejbModule/service/armurerie/ForgeArme.java
@@ -0,0 +1,32 @@
+package service.armurerie;
+
+
+import javax.ejb.Remote;
+import javax.ejb.Stateless;
+
+import clientServeur.IArme;
+import clientServeur.IForge;
+import entity.armurerie.Arme;
+
+
+/**
+ * Fabrique d'arme pour la création d'une arme
+ * @author OlivB
+ *
+ */
+@Stateless
+@Remote(IForge.class)
+public class ForgeArme implements IForge{
+
+	public IArme creerArme(){
+		return new Arme();
+	}
+	public IArme creerArme(String nom, int encombrement, int prix, String monnaie) {
+		return new Arme(nom, encombrement, prix, monnaie);
+	}
+	@Override
+	public IArme creerArme(int idArme, String nom, int encombrement, int prix, String monnaie) {
+		return new Arme(idArme, nom, encombrement, prix, monnaie);
+	}
+
+}

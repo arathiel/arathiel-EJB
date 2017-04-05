5c66f9e11933d270a7afd97a03434757e53c4b45
OllyB <tyno0809@gmail.com>
2017-04-05 09:27:29 +0200

    modif Entity ArmeJoueur

--- ejbModule/entity/armurerie/ArmeJoueur.java
+++ ejbModule/entity/armurerie/ArmeJoueur.java
@@ -13,34 +13,47 @@ import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 
-import armurerie.util.Etat;
+import util.armurerie.Etat;
+/**
+ * Classe ArmeJoueur représentatnt la table d'association entre Arme et Joueur avec comme attribut Etat de l'arme.
+ * Mappé en ManyToOne avec Arme et ManyToOne avec Joueur
+ * @author OlivB
+ *
+ */
 
 @Entity
 @Table(name="armeJoueur")
 public class ArmeJoueur implements Serializable{
-	
+
+	//Création d'un Id pour cette table composé de idArme, idJoueur et Etat. 1 seul etat par arme du joueur
 	@Embeddable
 	public static class IdArmeJoueur implements Serializable {
-		
+
 		private static final long serialVersionUID = 1L;
 		@Column(name="idArme", nullable = false)
 		private int idArme;
-		
+
 		@Column(name="idJoueur", nullable = false)
 		private int idJoueur;
 		
+		//String etat afin de typer la colonne en Varchar2 (sinon number si private Etat etat)
 		@Column(name="etat", nullable = false)
-		private Etat etat;
+		private String etat;
 
 		public IdArmeJoueur() {
 			super();
 		}
 
-		public IdArmeJoueur(int idArme, int idJoueur, Etat etat) {
+		public IdArmeJoueur(int idArme, int idJoueur, String etat) {
 			super();
 			this.idArme = idArme;
 			this.idJoueur = idJoueur;
 			this.etat = etat;
+		}
+
+		@Override
+		public String toString() {
+			return "IdArmeJoueur [idArme=" + idArme + ", idJoueur=" + idJoueur + ", etat=" + etat + "]";
 		}
 
 		public int getIdArme() {
@@ -59,56 +72,73 @@ public class ArmeJoueur implements Serializable{
 			this.idJoueur = idJoueur;
 		}
 
-		public Etat getEtat() {
+		public String getEtat() {
 			return etat;
 		}
 
-		public void setEtat(Etat etat) {
+		public void setEtat(String etat) {
 			this.etat = etat;
 		}
-				
+
 	}
-//Suite de la classe ArmeJoueur
+	//Suite de la classe ArmeJoueur
 	private static final long serialVersionUID = 1L;
-	
+
+	//instanciation de la Primary Key composite
 	@EmbeddedId
 	private IdArmeJoueur idArmeJoueur = new IdArmeJoueur();
-	
+
 	@ManyToOne(fetch = FetchType.EAGER)
 	@JoinColumn(name="idArme", insertable = false, updatable = false)
 	private Arme arme;
-	
+
 	@ManyToOne(fetch = FetchType.EAGER)
 	@JoinColumn(name="idJoueur", insertable = false, updatable = false)
 	private Joueur joueur;
-	
+
+	//@Enumerated car Etat est un enum et non une classe
 	@Enumerated(EnumType.STRING)
 	@Column(length = 10, insertable = false, updatable = false, nullable = false)
 	private Etat etat;
 
 	public ArmeJoueur() {
+
 		super();
 	}
 
 	public ArmeJoueur(Arme arme, Joueur joueur, Etat etat) {
 		getIdArmeJoueur().setIdArme(arme.getIdArme());
 		getIdArmeJoueur().setIdJoueur(joueur.getIdJoueur());
-		getIdArmeJoueur().setEtat(etat);
-		
+		getIdArmeJoueur().setEtat(Etat.getString(etat));
+
 		this.setArme(arme);
 		this.setJoueur(joueur);
 		this.setEtat(etat);
-		
+
 		arme.getArmeJoueur().add(this);
 		joueur.getArmeJoueur().add(this);
+	}
+
+	public ArmeJoueur(IdArmeJoueur idArmeJoueur, Arme arme, Joueur joueur, Etat etat) {
+		super();
+		this.idArmeJoueur = idArmeJoueur;
+		this.arme = arme;
+		this.joueur = joueur;
+		this.etat = etat;
 	}
 
 	public Arme getArme() {
 		return arme;
 	}
 
+	//Pour ajouter une arme dans l'instance de ArmeJoueur, nous devons créer idArmeJoueur si celui-ci est null
 	public void setArme(Arme arme) {
 		this.arme = arme;
+		if (arme!= null) {
+			if (idArmeJoueur==null) idArmeJoueur = new IdArmeJoueur();
+			this.idArmeJoueur.setIdArme(arme.getIdArme());
+		}
+
 	}
 
 	public Joueur getJoueur() {
@@ -117,6 +147,11 @@ public class ArmeJoueur implements Serializable{
 
 	public void setJoueur(Joueur joueur) {
 		this.joueur = joueur;
+		if (joueur!=null) {
+			if (idArmeJoueur==null) idArmeJoueur = new IdArmeJoueur();
+			this.idArmeJoueur.setIdJoueur(joueur.getIdJoueur());
+		}
+
 	}
 
 	public Etat getEtat() {
@@ -125,6 +160,8 @@ public class ArmeJoueur implements Serializable{
 
 	public void setEtat(Etat etat) {
 		this.etat = etat;
+		if (idArmeJoueur==null) idArmeJoueur = new IdArmeJoueur();
+		this.idArmeJoueur.setEtat(Etat.getString(etat));
 	}
 
 	public IdArmeJoueur getIdArmeJoueur() {
@@ -133,6 +170,12 @@ public class ArmeJoueur implements Serializable{
 
 	public void setIdArmeJoueur(IdArmeJoueur idArmeJoueur) {
 		this.idArmeJoueur = idArmeJoueur;
+	}
+
+	@Override
+	public String toString() {
+		return "ArmeJoueur [idArmeJoueur=" + idArmeJoueur + ", arme=" + arme + ", joueur=" + joueur + ", etat=" + etat
+				+ "]";
 	}
 
 }

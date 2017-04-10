fc079fb62049c081912320af6f6fcdcda4469de6
OllyB <tyno0809@gmail.com>
2017-04-10 14:03:05 +0200

    OK

--- insertJoueur.sql
+++
@@ -1,7 +0,0 @@
-/*fichier SQL pour l'insertion de nouveauxx joueurs*/
-
-insert into joueur values (1, 'BestPlayer');
-insert into joueur values (2, 'MiddlePlayer');
-insert into joueur values (3, 'BadPlayer');
-
-commit;
